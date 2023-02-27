7ff7157a4905f58ac0921d238a5eefb438a54f37
katerinakiseliova <kiseliova1985@mail.ru>
2023-02-27 12:37:26 +0300

    Task 1_1 ver 3

---
+++ Lesson_1/1.3/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a = int.Parse(Console.ReadLine()!);
+int b = -a;
+while (b <=a)
+{
+    Console.Write($"{b} ");
+    b ++;
+}
